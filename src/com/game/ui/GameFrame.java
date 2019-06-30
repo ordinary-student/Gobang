daec9be6477f4bca538ab2b9a54664c2e4c9cc5a
ordinary-student <52038952+ordinary-student@users.noreply.github.com>
2019-06-30 15:03:10 +0800

    Update GameFrame.java

--- src/com/game/ui/GameFrame.java
+++ src/com/game/ui/GameFrame.java
@@ -1,6 +1,14 @@
 package com.game.ui;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Menu;
+import java.awt.MenuBar;
+import java.awt.MenuItem;
+
+import javax.swing.JFrame;
 
 import com.game.utils.WindowUtil;
 
@@ -40,6 +48,33 @@ public class GameFrame extends KFrame
 		setLayout(null);
 		// 设置关闭方式
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		// 创建菜单栏
+		MenuBar mb = new MenuBar();
+		mb.setFont(new Font("粗体", Font.CENTER_BASELINE, 14));
+		// 创建菜单
+		Menu m1 = new Menu("开始");
+		Menu m2 = new Menu("帮助");
+		// m1.setFont(new Font("仿宋", Font.CENTER_BASELINE, 14));
+		// m2.setFont(new Font("仿宋", Font.CENTER_BASELINE, 14));
+		// 创建菜单项
+		final MenuItem mi1 = new MenuItem("新游戏");
+		final MenuItem mi2 = new MenuItem("退出游戏");
+		final MenuItem mi3 = new MenuItem("悔棋");
+		final MenuItem mi4 = new MenuItem("静音");
+
+		m1.add(mi1);
+		m1.add(mi2);
+		m2.add(mi3);
+		m2.add(mi4);
+		mb.add(m1);
+		mb.add(m2);
+		// 设置菜单栏
+		this.setMenuBar(mb);
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);// 关闭按钮
+		Container con = this.getContentPane();
+		con.setLayout(new BorderLayout());
 
 		validate();
 		// 显示界面
